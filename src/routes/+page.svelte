5041f71fd5c35c2854aa8868afd9086d245f75e4
Markus Härtig <markus.haertig@vantaio.com>
2025-01-14 12:36:56 +0100

    tiny css update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -20,12 +20,6 @@
   <div class="pb-8">
     <SLIDER></SLIDER>
   </div>
-
-  <h1>Welcome to SvelteKit</h1>
-  <p class="p2">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque veritatis doloribus ratione magni labore voluptatum totam a exercitationem quis incidunt
-    accusamus cum reprehenderit tenetur velit, sint commodi ab saepe ipsam?
-  </p>
   <div class="bigSpacer"></div>
   {#each data.contents as snippet}
     <h2>{snippet.headline}</h2>
