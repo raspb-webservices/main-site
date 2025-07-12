e343a809929c4466fea268a385ca99bf1b457c7e
Markus Härtig <markus.haertig@vantaio.com>
2025-07-12 13:10:19 +0200

    auth0 env

--- src/auth_config.ts
+++ src/auth_config.ts
@@ -1,12 +1,11 @@
-import { PUBLIC_AUTH0_DOMAIN, PUBLIC_AUTH0_CLIENT_ID  } from '$env/static/public';
 interface AuthConfig {
   domain: string;
   clientId: string;
 }
 
 const authConfig: AuthConfig = {
-  domain: PUBLIC_AUTH0_DOMAIN,
-  clientId: PUBLIC_AUTH0_CLIENT_ID
+  domain: import.meta.env.VITE_AUTH0_DOMAIN || '',
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID || ''
 };
 
 // Validierung hinzufügen
