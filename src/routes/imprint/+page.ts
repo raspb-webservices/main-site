66ddc696b4975d4366835f25cf53d73b52992e33
raspb-webservices <kontakt@raspb.de>
2025-09-15 12:54:47 +0200

    addition

--- src/routes/imprint/+page.ts
+++ src/routes/imprint/+page.ts
@@ -1,1 +1,11 @@
-export const prerender = true;
+import { addMessages } from 'svelte-i18n';
+
+export async function load() {
+  const pageMessagesDe = (await import(`$lib/i18n/locales/de/contact.json`)).default;
+  const pageMessagesEn = (await import(`$lib/i18n/locales/en/contact.json`)).default;
+
+  addMessages('de', pageMessagesDe);
+  addMessages('en', pageMessagesEn);
+
+  return {};
+}
