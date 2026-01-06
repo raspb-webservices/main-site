32d7cb3796042549759cfe15c0e3cb51f52edfe0
raspb-webservices <kontakt@raspb.de>
2026-01-06 14:46:41 +0100

    removes bot for now

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -63,8 +63,6 @@
       {@render children?.()}
     </main>
     <FOOTER />
-    <elevenlabs-convai agent-id="agent_6201k7sa8fxafb7r2g21x6c1xnr1"></elevenlabs-convai>
-    <script src="https://unpkg.com/@elevenlabs/convai-widget-embed" async type="text/javascript"></script>
   {/if}
   <CookieConsentComponent />
 </div>
