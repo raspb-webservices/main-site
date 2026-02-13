660f2c15624d1b815e1bc627152d27cc2add959d
raspb-webservices <kontakt@raspb.de>
2026-02-13 17:08:02 +0100

    fix 124

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,10 +39,10 @@
   <link rel="preload" href="$fonts/circular/CircularStd-Light.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
   <link rel="preload" href="$fonts/circular/CircularStd-Medium.woff2" as="font" type="font/woff2" crossorigin="anonymous" />
   <link rel="preconnect" href="https://cdn.jsdelivr.net" crossorigin="anonymous" />
-  <link rel="icon" type="image/png" href="/icons/favicon-96x96.png" sizes="96x96" />
-  <link rel="icon" type="image/svg+xml" href="/icons/favicon.svg" />
-  <link rel="shortcut icon" href="/icons/favicon.ico" />
-  <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
+  <link rel="icon" type="image/png" href="$icons/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="$icons/favicon.svg" />
+  <link rel="shortcut icon" href="$icons/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="$icons/apple-touch-icon.png" />
   <meta name="apple-mobile-web-app-title" content="raspb" />
   <meta property="og:type" content="website" />
   <meta property="og:site_name" content="raspb" />
